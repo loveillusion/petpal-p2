1c3f449abf5d59b8a16ec16c59965655e9532d69
allenxu <allenx.03819@gmail.com>
2023-11-14 17:32:58 -0500

    update account url

--- petpal/accounts/urls.py
+++ petpal/accounts/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from .views import SeekerRegisterView, SeekerLoginView, SeekerProfileView, SeekerEditProfileView
-from .views import ShelterRegisterView, ShelterLoginView, ShelterProfileView, ShelterEditProfileView
+from .views import SeekerRegisterView, SeekerLoginView, SeekerProfileView, SeekerEditProfileView, SeekerDeleteView
+from .views import ShelterRegisterView, ShelterLoginView, ShelterProfileView, ShelterEditProfileView, ShelterDeleteView
 
 
 appname = 'accounts'
@@ -9,8 +9,10 @@ urlpatterns = [
     path('user/login/', SeekerLoginView.as_view(), name='seeker_login'),
     path('user/profile/', SeekerProfileView.as_view(), name='seeker_profile'),
     path('user/profile/management/', SeekerEditProfileView.as_view(), name='seeker_edit_profile'),
+    path('user/profile/delete/', SeekerDeleteView.as_view(), name='seeker_delete_profile'),
     path('shelter/new/', ShelterRegisterView.as_view(), name='shelter_register'),
     path('shelter/login/', ShelterLoginView.as_view(), name='shelter_login'),
     path('shelter/profile/', ShelterProfileView.as_view(), name='shelter_profile'),
     path('shelter/profile/management/', ShelterEditProfileView.as_view(), name='shelter_edit_profile'),
+    path('shelter/profile/delete/', ShelterDeleteView.as_view(), name='shelter_delete_profile')
 ]
