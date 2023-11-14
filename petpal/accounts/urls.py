9b0a3c57b67c3550fcf1e961a7c042e2486798e4
allenxu <allenx.03819@gmail.com>
2023-11-13 20:13:32 -0500

    Fixed accounts views and urls

--- petpal/accounts/urls.py
+++ petpal/accounts/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import SeekerRegisterView, SeekerLoginView, SeekerProfileView, SeekerEditProfileView, SeekerUploadProfilePictureView
-from .views import ShelterRegisterView, ShelterLoginView, ShelterProfileView, ShelterEditProfileView, ShelterUploadProfilePictureView
+from .views import SeekerRegisterView, SeekerLoginView, SeekerProfileView, SeekerEditProfileView
+from .views import ShelterRegisterView, ShelterLoginView, ShelterProfileView, ShelterEditProfileView
+
 
 appname = 'accounts'
 urlpatterns = [
@@ -8,10 +9,8 @@ urlpatterns = [
     path('user/login/', SeekerLoginView.as_view(), name='seeker_login'),
     path('user/profile/', SeekerProfileView.as_view(), name='seeker_profile'),
     path('user/profile/management/', SeekerEditProfileView.as_view(), name='seeker_edit_profile'),
-    path('user/profile/picture/', SeekerUploadProfilePictureView.as_view(), name='seeker_upload_picture'),
     path('shelter/new/', ShelterRegisterView.as_view(), name='shelter_register'),
     path('shelter/login/', ShelterLoginView.as_view(), name='shelter_login'),
     path('shelter/profile/', ShelterProfileView.as_view(), name='shelter_profile'),
     path('shelter/profile/management/', ShelterEditProfileView.as_view(), name='shelter_edit_profile'),
-    path('shelter/profile/picture/', ShelterUploadProfilePictureView.as_view(), name='shelter_upload_picture'),
 ]
