8311f36b5a0157948fd5c7347ec564435e2a1ff2
Leo <liuyuanyi1208@gmail.com>
2023-11-15 16:33:05 -0500

    Fix setting up environment

--- run.sh
+++ run.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 # Activate the virtual environment
-# This assumes that your virtual environment is in the 'venv' directory.
 source venv/bin/activate
 
 # Navigate to the directory where manage.py is located
