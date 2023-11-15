e12cd15d2dc47582544a885eb8bcee557cf64f77
KevinZ <stardustinglove@gmail.com>
2023-11-15 14:55:36 -0500

    fixed search filters and some url endpoints

--- petpal/accounts/urls.py
+++ petpal/accounts/urls.py
@@ -5,14 +5,14 @@ from .views import ShelterRegisterView, ShelterLoginView, ShelterProfileView, Sh
 
 appname = 'accounts'
 urlpatterns = [
-    path('user/new/', SeekerRegisterView.as_view(), name='seeker_register'),
+    path('user/signup/', SeekerRegisterView.as_view(), name='seeker_register'),
     path('user/login/', SeekerLoginView.as_view(), name='seeker_login'),
     path('user/profile/', SeekerProfileView.as_view(), name='seeker_profile'),
     path('user/profile/management/', SeekerEditProfileView.as_view(), name='seeker_edit_profile'),
-    path('user/profile/delete/', SeekerDeleteView.as_view(), name='seeker_delete_profile'),
-    path('shelter/new/', ShelterRegisterView.as_view(), name='shelter_register'),
+    path('user/profile/deletion/', SeekerDeleteView.as_view(), name='seeker_delete_profile'),
+    path('shelter/signup/', ShelterRegisterView.as_view(), name='shelter_register'),
     path('shelter/login/', ShelterLoginView.as_view(), name='shelter_login'),
     path('shelter/profile/', ShelterProfileView.as_view(), name='shelter_profile'),
     path('shelter/profile/management/', ShelterEditProfileView.as_view(), name='shelter_edit_profile'),
-    path('shelter/profile/delete/', ShelterDeleteView.as_view(), name='shelter_delete_profile')
+    path('shelter/profile/deletion/', ShelterDeleteView.as_view(), name='shelter_delete_profile')
 ]
--- petpal/search/views.py
+++ petpal/search/views.py
@@ -1,10 +1,16 @@
 from rest_framework import generics, filters
 from shelter.models import Pet
-from shelter.serializers import PetSerializer
+from .serializers import PetSearchSerializer
 
 
 class PetSearchView(generics.ListAPIView):
     queryset = Pet.objects.all()
-    serializer_class = PetSerializer
-    filter_backends = [filters.SearchFilter]
-    search_fields = ['name', 'gender', 'breed', 'age']
+    serializer_class = PetSearchSerializer
+    filter_backends = [filters.SearchFilter, filters.OrderingFilter]
+    search_fields = ['shelter__name', 'name', 'gender', 'breed', 'age', 'size', 'description', 'is_adopted']
+    ordering_fields = ['name', 'age']
+    # To sort by 'name' in ascending order: http://localhost:8000/search/?ordering=name
+    # To sort by 'name' in descending order: http://localhost:8000/search/?ordering=-name
+    # To sort by 'age' in ascending order: http://localhost:8000/search/?ordering=age
+    # To sort by 'age' in descending order: http://localhost:8000/search/?ordering=-age
+    ordering = ['name']  # default sorting
